4523f1133d041cb4557ea1b3464b8c8a5cdae26b
Cintya Karina Banda <karibanda10@gmail.com>
2024-01-16 12:27:21 -0600

    Nueva Barra

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
-import Link from "next/link"
+import styles from "@/styles/styles.module.css"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/pages/test">Test</Link></li>
-        <li><Link href="/pages/testDos">Segunda Opción</Link></li>
-      </ul>
-    </main>
+    <div className={styles.container}>
+      {/*}<Head>
+        <title>Mi Página web</title>
+        <meta httpEquiv="Context-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+      </Head>{*/}
+      {/*Navbar*/}
+      <nav className={styles.navbar}>
+        <a href="#" className={styles.navbarBrand}>Home</a>
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Ayuda</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Sesion</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Contenido principal */}
+      <div className={styles.content}>
+        <h1>Hola Mundo</h1>
+      </div>
+      <Footer/>
+    </div>
   )
 }
