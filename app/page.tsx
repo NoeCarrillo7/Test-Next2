6bfbb283023bac91ff5503491a02ee0426d696ef
Cintya Karina Banda <karibanda10@gmail.com>
2024-01-16 18:55:53 -0600

    Menu Funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/styles.module.css"
 import Footer from "@/components/Footer"
+import Menu from "@/components/Menu"
 
 export default function Home() {
   return (
@@ -9,19 +10,8 @@ export default function Home() {
         <meta httpEquiv="Context-Type" content="text/html;charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
       </Head>{*/}
-      {/*Navbar*/}
-      <nav className={styles.navbar}>
-        <a href="#" className={styles.navbarBrand}>Home</a>
-        <ul className={styles.navbarNav}>
-          <li className={styles.navItem}>
-            <a href="#" className={styles.navLink}>Ayuda</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="#" className={styles.navLink}>Sesion</a>
-          </li>
-        </ul>
-      </nav>
-
+      
+      <Menu/>
       {/* Contenido principal */}
       <div className={styles.content}>
         <h1>Hola Mundo</h1>
