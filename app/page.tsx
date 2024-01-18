c663fcfd963e07e77da6b6eb347fe70070f32e05
Cintya Karina Banda <karibanda10@gmail.com>
2024-01-18 14:00:32 -0600

    Imagen y Datos

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import styles from "@/styles/styles.module.css"
 import Footer from "@/components/Footer"
 import Menu from "@/components/Menu"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       {/*}<Head>
         <title>Mi Página web</title>
         <meta httpEquiv="Context-Type" content="text/html;charset=UTF-8" />
@@ -14,9 +15,16 @@ export default function Home() {
       <Menu/>
       {/* Contenido principal */}
       <div className={styles.content}>
-        <h1>Hola Mundo</h1>
+        <h1>Cintya Karina Banda González</h1>
+        <h1>_______________________________________</h1>
+        <h1>Redes Sociales</h1>
+        <ul>Facebook: KariiBanda</ul>
+        <ul>Github: CintyaKarinaBanda</ul>
+        
       </div>
+      <Image src={"/descarga.png"} alt="imagen" width={500} height={500}/>
+
       <Footer/>
-    </div>
+    </>
   )
 }
